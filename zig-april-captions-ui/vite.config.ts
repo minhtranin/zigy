dfc4a42f3a39d592c8a6542ec52a259c899758a9
MinhCongTran <minh@wearebloom.com.au>
2025-12-10 00:06:55 +0700

    feat: add tailwind css

--- zig-april-captions-ui/vite.config.ts
+++ zig-april-captions-ui/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -7,7 +8,8 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [
-    react()
+    react(),
+    tailwindcss()
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
